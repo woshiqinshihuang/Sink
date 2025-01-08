0a6c09816ba60a925c345fa029dde9e10198ec1d
我是秦始皇 <155155820+woshiqinshihuang@users.noreply.github.com>
2025-01-08 15:09:56 +0800

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -7,11 +7,6 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
 <template>
   <section class="md:pt-6">
     <div class="container flex flex-col items-center py-8 mx-auto sm:flex-row">
-      <a
-        href="/"
-        class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
-        :title="title"
-      >{{ title }}</a>
       <span
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
         target="_blank"
