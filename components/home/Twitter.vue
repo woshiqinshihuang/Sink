c15ea2e90f8512d9669bc6b2268e386b9310ffdf
我是秦始皇 <155155820+woshiqinshihuang@users.noreply.github.com>
2025-01-08 14:58:54 +0800

    Update Twitter.vue

--- components/home/Twitter.vue
+++ components/home/Twitter.vue
@@ -6,18 +6,5 @@ const { twitter } = useAppConfig()
 </script>
 
 <template>
-  <a
-    :href="twitter"
-    target="_blank"
-    title="X(Twitter)"
-    class="inline-flex items-center px-3 py-1 mx-auto my-4 space-x-1 text-sm font-medium rounded-lg bg-muted"
-  >
-    <XIcon class="w-4 h-4" />
-    <Separator
-      orientation="vertical"
-      class="h-4"
-    />
-    <span>Follow me on X(Twitter)</span>
-    <ArrowRight class="w-4 h-4" />
-  </a>
+
 </template>
